370772be6f95cbe36c3b8dde6aeae5577a828fc0
Alicia Fritsche <alicia.fritsche@web.de>
2023-09-07 19:07:57 +0100

    Remove robot header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="robots" content="all" />
       </Head>
 
       <div className="page">
