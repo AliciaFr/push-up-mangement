2d8576d3615d4ea722a2577726c74534515de55c
Alicia Fritsche <alicia.fritsche@web.de>
2023-09-07 18:20:42 +0100

    add Head with meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,36 @@ import PageTitle from "./components/PageTitle";
 import AdditionalImages from "./components/AdditionalImages";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 import ParagraphImage from "./components/Image";
 
 export default function Home() {
   return (
-    <div className="page">
-      <PageTitle />
-      <PageDivider />
-      <ParagraphImage
-        src="/image1.jpg"
-        alt="Hero Bild mit Dominik Hülshorst der Schlagzeug auf einer Bühne spielt"
-        className="hero"
-      />
-      <PageDivider />
-      <Services />
-      <PageDivider />
-      <AdditionalImages />
-      <PageDivider />
-      <Contact />
-      <PageDivider />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="all" />
+      </Head>
+
+      <div className="page">
+        <PageTitle />
+        <PageDivider />
+        <ParagraphImage
+          src="/image1.jpg"
+          alt="Hero Bild mit Dominik Hülshorst der Schlagzeug auf einer Bühne spielt"
+          className="hero"
+        />
+        <PageDivider />
+        <Services />
+        <PageDivider />
+        <AdditionalImages />
+        <PageDivider />
+        <Contact />
+        <PageDivider />
+        <Footer />
+      </div>
+    </>
   );
 }
+
